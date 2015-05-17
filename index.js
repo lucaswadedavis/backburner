79e0e57c86121c0ea86de22779e0a76a5b890389
Luke Davis <luke@peopleofthebit.com>
2015-05-16 21:43:38 -0700

    uses canvas to create an image that's made to be the desktop background

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 var linuxWallpaper = require('linux-wallpaper');
 var backburner = {};
 var path = "./images/WeAreNotTakingTheWizard.jpg"
+path = "./wallpaper.png";
+var screen = {width:1366, height: 760};
 
+var Canvas = require('canvas')
+  , Image = Canvas.Image
+    , canvas = new Canvas(screen.width,screen.height)
+	  , ctx = canvas.getContext('2d');
+	   
+	  ctx.font = '30px Impact';
+	  //ctx.rotate(.1);
+	  ctx.fillText("Awesome!", screen.width/2, screen.height/2);
+	   
+	  var te = ctx.measureText('Backburner!');
+	  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+	  ctx.beginPath();
+	  ctx.lineTo(50, 102);
+	  ctx.lineTo(50 + te.width, 102);
+	  ctx.stroke();
+	   
+	  //console.log('<img src="' + canvas.toDataURL() + '" />');
+
+	  var fs = require('fs')
+	    , out = fs.createWriteStream(__dirname + '/wallpaper.png')
+	      , stream = canvas.pngStream();
+	       
+	      stream.on('data', function(chunk){
+		        out.write(chunk);
+	      });
+ 
+stream.on('end', function(){
+	  console.log('saved png');
+	linuxWallpaper.set(path,function(err){
+		if(err){
+			console.log(err);
+		} else {
+			console.log('it worked?');
+		}
+	});
+});
+
+/*
 linuxWallpaper.set(path,function(err){
 	if (err){
 		console.log(err);
@@ -10,5 +50,5 @@ linuxWallpaper.set(path,function(err){
 	}
 });
 
-
+*/
 module.exports = backburner;
